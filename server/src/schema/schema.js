d38eac2e3cf4d415959c717a282fd408d2a659d9
Ivan <ivanakazver3d@gmail.com>
2019-01-23 18:12:01 +0700

    Add RootQuery to schema, use dummy data for its functionality for now

--- server/src/schema/schema.js
+++ server/src/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// Dummy data
+const books = [
+  { id: "1", name: "The book", genre: "Sci-Fi" },
+  { id: "2", name: "The Great Book of Winterhold", genre: "Poetry" },
+  { id: "3", name: "Song of ice and fire", genre: "Fantasy" }
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -9,4 +17,22 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to fetch book from database
+        return _.find(books, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
