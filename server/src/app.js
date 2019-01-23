25cb0dc6368139922173fba3ac5ee365880545bc
Ivan <ivanakazver3d@gmail.com>
2019-01-23 18:33:28 +0700

    Change id type from string to id, graphiql set to true

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ schema }));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
