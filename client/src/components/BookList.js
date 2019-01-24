63312aa9239a21849fd3bc9a7796336cefe5ab22
Ivan <ivanakazver3d@gmail.com>
2019-01-24 16:22:00 +0700

    Delete all things that won't be used in this project, add BookList component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  state = {  }
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
